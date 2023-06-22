70f7d28b940a802526d117aacfacb0b0016fcbb0
Roger Guimaraes <rogersgti87@gmail.com>
2023-06-22 13:54:10 -0300

    220623

--- app/Http/Controllers/Backend/CustomerServicesController.php
+++ app/Http/Controllers/Backend/CustomerServicesController.php
@@ -205,7 +205,7 @@ class CustomerServicesController extends Controller
 
         if($invoice->payment_method == 'Pix'){
           $getInfoPixPayment      = Invoice::verifyStatusPixPayment($verifyTransaction->transaction_id);
-          $image = $getInfoPixPayment->point_of_interaction->transaction_data->qr_code_base64;
+          $image = $getInfoPixPayment->qr_code_base64;
           $imageName = $invoice->id.'.'.'png';
           \File::put(public_path(). '/pix/' . $imageName, base64_decode($image));
         }else{
